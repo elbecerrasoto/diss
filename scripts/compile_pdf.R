9f5e1053cb9a145662eb209517ffea6bd2a19ae1
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2020-09-06 10:17:02 -0600

    v2.1

--- scripts/compile_pdf.R
+++ scripts/compile_pdf.R
@@ -1,5 +1,9 @@
 #!/usr/bin/env Rscript
 
-main_file <- 'becerra_emanuel_CARLv2.0.Rmd'
+# Deletes the .Rmd in case of error
+# Should be the same name as in _bookdown.yml
+main_file <- 'becerra_emanuel_CARLv2.1.Rmd'
 unlink(main_file)
+
+# Compiles Book
 bookdown::render_book("index.Rmd")
